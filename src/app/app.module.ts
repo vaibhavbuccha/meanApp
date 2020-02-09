528843466498dd060069b484231b3e8483b223c6
vaibhavbuccha <jainvaibhav415@gmail.com>
2020-02-09 21:25:29 +0530

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { postCreateComponent } from './posts/post-create/post-create.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    postCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './post-create.component.html'
+})
+
+export class postCreateComponent {
+
+}
